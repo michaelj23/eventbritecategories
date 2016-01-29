5363c38d7a1c53a8ad6abec33cc60241554cc516
Michael <mju@berkeley.edu>
2016-01-28 23:57:54 -0800

    Implement index view and template, with API call to categories

--- eventbrite/categories/views.py
+++ eventbrite/categories/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
+import requests
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello world')
+	response = requests.get(
+		'https://www.eventbriteapi.com/v3/categories/',
+		headers = {
+			'Authorization': 'Bearer 56UBOZLZ4CLCT7JWUQ76'
+		},
+		verify = True
+	)
+	return render(request, 'categories/index.html', {'categories': response.json()['categories']})
+	return JsonResponse(response.json())
