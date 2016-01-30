65cf7046f119c540e752b34fec2472be5dc3dabe
Michael <mju@berkeley.edu>
2016-01-29 21:48:05 -0800

    Add view, template, and API call for finding relevant events

--- eventbrite/categories/urls.py
+++ eventbrite/categories/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'categories'
+
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^events', views.events, name='events')
 ]
--- eventbrite/categories/views.py
+++ eventbrite/categories/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 import requests
 # Create your views here.
 def index(request):
+	# TODO: check for errors in request
+	# TODO: error messages, check that user only chooses 3 events
 	response = requests.get(
 		'https://www.eventbriteapi.com/v3/categories/',
 		headers = {
@@ -12,4 +15,20 @@ def index(request):
 		verify = True
 	)
 	return render(request, 'categories/index.html', {'categories': response.json()['categories']})
-	return JsonResponse(response.json())
+
+def events(request):
+	if 'category' not in request.GET:
+		return HttpResponseRedirect(reverse('categories:index'))
+	# TODO: sort by date, maybe try to show events' times in user's timezone?
+	# TODO: pagination
+	response = requests.get(
+		'https://www.eventbriteapi.com/v3/events/search/',
+		params = {
+			'categories': request.GET['category']
+		},
+		headers = {
+			'Authorization': 'Bearer 56UBOZLZ4CLCT7JWUQ76'		
+		},
+		verify = True
+	)
+	return render(request, 'categories/events.html', {'events': response.json()['events']})
